6f5e6095b73e182dddb1f22c53f2d3a64bed8eca
abidikhairi <abidikhairi.1337@gmail.com>
2023-11-15 20:55:07 +0100

    update run.sh: update architecture + verbose mode

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python main.py --d-model 512 --nhead 4 --dropout 0.6 --dim-ff 256 --num-layers 2 \
+set -x
+
+python main.py --d-model 512 --nhead 4 --dropout 0.6 --dim-ff 256 --num-layers 4 \
     --learning-rate 0.003 --betas 0.9 0.998 --warmup-steps 500 --batch-size 16 --num-workers 3 \
     --training-file data/refseq/human_proteome_training.txt \
     --validation-file data/refseq/human_proteome_validation.txt \
